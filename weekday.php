cf0fc9bce1a3e830576ae2f152e300aea225f3fa
riegerc <ch_ri@gmx.at>
2019-08-29 16:08:40 +0200

    fix double test

--- weekday.php
+++ weekday.php
@@ -6,34 +6,43 @@
 
 setlocale(LC_TIME, 'de_AT.utf-8');
     //TODO: find something to do
-$day = $argv[1];
-$month = $argv[2];
-$year = $argv[3]; /* muss vierstellig sein */
+main($argc,$argv);
 
-if($argc<4 || $argc>5) {
-    echo "Wrong number of arguments.";
-    exit(1);
+function main($argc,$argv): void {
+    list($day, $month, $year) = argumentHandling($argc, $argv);
+    $m = (($month - 2 - 1) + 12) % 12 + 1; // this is because of the modulo
+    if ($m >= 11) {
+        $c = substr($year - 1, 0, 2);
+        $y = substr($year - 1, 2, 2);
+    }else{
+        $c = substr($year, 0, 2);
+        $y = substr($year, 2, 2);
+    }
+
+    $weekdayNumber = ($day + intval(2.6 * $m - 0.2) + $y + intval($y / 4) + intval($c / 4) - 2 * $c) % 7;
+    $weekday = getWeekdayForWeekdayNumber($weekdayNumber);
+    printEingabe($day, $month, $year);
+    printPhpFunction($year, $month, $day);
+    printAlgorythmus($weekday);
+    printDebug($m, $y, $c);
 }
 
-$m = (($month - 2 - 1 ) + 12 ) % 12 + 1 ; // this is because of the modulo
-$c = substr($year, 0, 2);
-if($m>=11) {
-    $c = substr($year-1, 0, 2);
+/**
+ * @param $argc
+ * @param $argv
+ * @return array
+ */
+function argumentHandling($argc, $argv): array {
+    $day = $argv[1];
+    $month = $argv[2];
+    $year = $argv[3]; /* muss vierstellig sein */
+
+    if ($argc < 4 || $argc > 5) {
+        echo "Wrong number of arguments.";
+        exit(1);
+    }
+    return [$day, $month, $year];
 }
-$y = substr($year, 2, 2);
-if($m>=11) {
-    $y = substr($year-1, 2, 2);
-}
-
-$weekdayNumber = ($day + intval (2.6 * $m - 0.2) + $y  + intval ($y/4) + intval ($c/4) - 2*$c ) % 7;
-
-$weekday = getWeekdayForWeekdayNumber($weekdayNumber);
-
-
-printEingabe($day, $month, $year);
-printPhpFunction($year, $month, $day);
-printAlgorythmus($weekday);
-printDebug($m, $y, $c);
 
 /**
  * @param int $m
